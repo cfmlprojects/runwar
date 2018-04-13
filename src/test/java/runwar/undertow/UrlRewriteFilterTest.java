8dced3ee05bc5b538ef8dbf3992068e830bc9b0d
cfmlprojects <cfml@cfmlprojects.org>
2018-04-12 19:00:22 -0600

    Update urlrewrite filter.  Closes #117, Closes #122, Closes #119

--- src/test/java/runwar/undertow/UrlRewriteFilterTest.java
+++ src/test/java/runwar/undertow/UrlRewriteFilterTest.java
@@ -1,37 +1,35 @@
 package runwar.undertow;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-
-import javax.servlet.ServletException;
-
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.tuckey.web.filters.urlrewrite.Conf;
 import org.tuckey.web.filters.urlrewrite.NormalRewrittenUrl;
 import org.tuckey.web.filters.urlrewrite.UrlRewriter;
 import org.tuckey.web.filters.urlrewrite.utils.Log;
-
-import lucee.loader.servlet.CFMLServlet;
 import testutils.mock.MockFilterChain;
 import testutils.mock.MockRequest;
 import testutils.mock.MockResponse;
 import testutils.mock.MockServletContext;
 
+import javax.servlet.ServletException;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class UrlRewriteFilterTest {
-    CFMLServlet servlet;
 
     MockResponse response;
     MockRequest request;
     testutils.mock.MockServletContext servletContext;
     MockFilterChain chain;
 
-    public UrlRewriteFilterTest() {
+    UrlRewriteFilterTest() {
         Log.setLevel("DEBUG");
         response = new MockResponse();
         request = new MockRequest("/");
@@ -40,10 +38,11 @@ public class UrlRewriteFilterTest {
     }
 
     @Test
-    public void simpleApacheHConf() throws IOException, ServletException, InvocationTargetException {
-        Conf conf = new Conf(servletContext, new FileInputStream(new File("src/test/resources/urlrewrite.htaccess")), "urlrewrite.htaccess", null, true);
+    @DisplayName("Should test a simple mod_rewrite style .htaccess conf")
+    void simpleApacheHConf() throws IOException, ServletException, InvocationTargetException {
+        Conf conf = new Conf(servletContext, new FileInputStream(new File("src/test/resources/urlrewrite/simple.htaccess")), "simple.htaccess", null, true);
         assertTrue(conf.isEngineEnabled());
-        assertTrue(conf.isOk());
+        assertTrue(conf.isOk(),"conf did not parse");
         assertTrue(conf.getRules().size() > 0);
         NormalRewrittenUrl rewrittenRequest;
         MockRequest request;
@@ -55,6 +54,53 @@ public class UrlRewriteFilterTest {
         assertNotNull(rewrittenRequest);
         assertEquals("index.cfm/notthere", rewrittenRequest.getTarget());
 
+        request = new MockRequest("/foo/not/there");
+        rewrittenRequest = (NormalRewrittenUrl) urlRewriter.processRequest(request, response);
+        assertNotNull(rewrittenRequest);
+        assertEquals("index.cfm/foo/not/there", rewrittenRequest.getTarget());
+    }
+
+    @Test
+    @DisplayName("Should test relative paths of rewrite rule")
+    void relativePathTest() throws IOException, ServletException, InvocationTargetException {
+        HashMap<String,String> requestTargets = new HashMap<>();
+        String rule = "RewriteRule ^.*$ rewritten.html";
+        requestTargets.put("/index.cfm","rewritten.html");
+        requestTargets.put("/foo/index.cfm","rewritten.html");
+        requestTargets.put("/bar/index.cfm","rewritten.html");
+        assertRuleResult(rule, requestTargets);
+        
+        requestTargets = new HashMap<>();
+        rule = "RewriteRule ^.*$ /rewritten.html";
+        requestTargets.put("/index.cfm","/rewritten.html");
+        requestTargets.put("/foo/index.cfm","/rewritten.html");
+        requestTargets.put("/bar/index.cfm","/rewritten.html");
+        assertRuleResult(rule, requestTargets);
+
+        requestTargets = new HashMap<>();
+        rule = "RewriteRule .* 410.html [L]";
+        requestTargets.put("/index.cfm","410.html");
+        assertRuleResult(rule, requestTargets);
+        
+    }
+
+    private void assertRuleResult(String rule, HashMap<String,String> requestTargets) {
+        Conf conf = new Conf(servletContext, new ByteArrayInputStream(rule.getBytes()), "simple.htaccess", null, true);
+        assertTrue(conf.isEngineEnabled());
+        assertTrue(conf.isOk(),"conf did not parse");
+        assertTrue(conf.getRules().size() > 0);
+
+        final UrlRewriter urlRewriter = new UrlRewriter(conf);
+
+        requestTargets.forEach((path, result) -> {
+            MockRequest mockRequest = new MockRequest(path);
+            try {
+                NormalRewrittenUrl rewrittenRequest = (NormalRewrittenUrl) urlRewriter.processRequest(mockRequest, response);
+                assertEquals(result, rewrittenRequest.getTarget());
+            } catch (InvocationTargetException | IOException | ServletException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     class MockNormalRewrittenUrl extends NormalRewrittenUrl {
