c73eb35bb79a8eee41989888e674d502cfcc3588
Brad Wood <brad@bradwood.com>
2020-10-23 13:07:29 -0500

    Even more extensions

--- src/main/java/runwar/RunwarConfigurer.java
+++ src/main/java/runwar/RunwarConfigurer.java
@@ -416,7 +416,7 @@ class RunwarConfigurer {
         */
 
         // Default list of what the default servlet will serve
-        String allowedExt = "3gp,7z,aif,aiff,aifc,aac,apk,asf,au,atom,avi,bak,bk,bz2,bin,bmp,btm,css,csv,cdr,cmx,dat,deb,dtd,dll,dmg,doc,docx,eml,eps,exe,flv,fla,font,gif,gz,gzip,htm,html,iso,ico,ipa,img,ini,ia,indd,jar,jpeg,jpg,js,json,hey,lz,m4v,map,maf,markdown,md,mid,midi,mkv,mov,mp1,mp2,mp3,mp4,mpg,mpe,mpeg,mpeg4,msi,odt,ott,odg,odf,ots,ogg,pdf,png,pps,ppt,pptx,pot,psd,pmd,pub,ra,rar,raw,rpm,rtf,rss,svg,svgz,sdd,swf,tar,tif,tiff,ttf,tsv,txt,wav,wmv,wmf,xcf,xls,xlsx,xhtml,xml,yml,yaml,zip,woff,woff2,eot,webm";
+        String allowedExt = "3gp,3gpp,7z,ai,aif,aiff,asf,asx,atom,au,avi,bin,bmp,btm,cco,crt,css,csv,deb,der,dmg,doc,docx,eot,eps,flv,font,gif,hqx,htc,htm,html,ico,img,ini,iso,jad,jng,jnlp,jpeg,jpg,js,json,kar,kml,kmz,m3u8,m4a,m4v,map,mid,midi,mml,mng,mov,mp3,mp4,mpeg,mpeg4,mpg,msi,msm,msp,ogg,otf,pdb,pdf,pem,pl,pm,png,ppt,pptx,prc,ps,psd,ra,rar,rpm,rss,rtf,run,sea,shtml,sit,svg,svgz,swf,tar,tcl,tif,tiff,tk,ts,ttf,txt,wav,wbmp,webm,webp,wmf,wml,wmlc,wmv,woff,woff2,xhtml,xls,xlsx,xml,xpi,xspf,zip,aifc,aac,apk,bak,bk,bz2,cdr,cmx,dat,dtd,eml,fla,gz,gzip,ipa,ia,indd,hey,lz,maf,markdown,md,mkv,mp1,mp2,mpe,odt,ott,odg,odf,ots,pps,pot,pmd,pub,raw,sdd,tsv,xcf,yml,yaml";
         // Add any custom additions by our users
         if( serverOptions.defaultServletAllowedExt().length() > 0 ) {
         	allowedExt += "," + serverOptions.defaultServletAllowedExt();
